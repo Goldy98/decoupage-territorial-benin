089eb1680786e5e837fd9e88620cfa1c4cd478dd
Goldy98 <sagboaime267@gmail.com>
2021-12-15 22:33:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# decoupage-territorial-benin
+
+[Package Node.js](https://bj-decoupage-territorial.herokuapp.com/) (non officielle) pour obtenir des informations de base sur les départements, communes, arrondissements et les quartiers du Bénin. Elle s'inspire du fichier du découpage territorial du Bénin proposé par [leplutonien](https://github.com/leplutonien/decoupage_territorial_benin).
+
+# Stack Technique
+
+- [Node.js](https://nodejs.org/en/)
+- [Typescript](https://www.typescriptlang.org/)
+
+# Tu as envie de donner un coup de pouce?
+
+> Bah il y a beaucoup à faire 😁
+
+- Ecrire les tests ( C'est con, mais c'est utile 😛 )
+- Améliorer la doc 📚
+- Rajouter des nouvelles infos/fonctions si nécéssaire, donc écrire du code 🏄
+- Mettre un petit star ⭐️ à ce dépôt.
+
+# Utilisé par :
+
+# License
+
+[MIT License](LICENSE.md)
---
+++ src/index.ts
@@ -0,0 +1,74 @@
+const dataFileContent = require("../decoupage_territorial_benin.json");
+
+let alreadyParsedData: Department[];
+
+export interface Department {
+  idDep: number;
+  labelDep: string;
+  communes: Commune[];
+}
+
+export interface Commune {
+  idCom: number;
+  labelCom: string;
+  arrondissements: Arrondissement[];
+}
+
+export interface Arrondissement {
+  idArrond: number;
+  labelArrond: string;
+  quartiers: Quartier[];
+}
+
+export interface Quartier {
+  idQuart: number;
+  labelQuart: string;
+}
+
+export function allBeninDepartments(): Department[] {
+  if (!alreadyParsedData)
+    alreadyParsedData = dataFileContent.map(
+      (rawDepartment: any): Department => {
+        return {
+          idDep: rawDepartment["id_dep"],
+          labelDep: rawDepartment["lib_dep"],
+          communes: rawDepartment["communes"].map((com: any) =>
+            getCommuneFromRawData(com)
+          )
+        };
+      }
+    );
+
+  return alreadyParsedData;
+}
+
+function getCommuneFromRawData(rawData: {
+  id_com: number;
+  lib_com: string;
+  arrondissements: any[];
+}): Commune {
+  return {
+    idCom: rawData["id_com"],
+    labelCom: rawData["lib_com"],
+    arrondissements: rawData.arrondissements.map(arr =>
+      getArrondissementFromRawData(arr)
+    )
+  };
+}
+
+function getArrondissementFromRawData(rawData: {
+  id_arrond: number;
+  lib_arrond: string;
+  quartiers: any[];
+}): Arrondissement {
+  return {
+    idArrond: rawData["id_arrond"],
+    labelArrond: rawData["lib_arrond"],
+    quartiers: rawData["quartiers"].map((rawQuart): Quartier => {
+      return {
+        idQuart: rawQuart["id_quart"],
+        labelQuart: rawQuart["lib_quart"]
+      };
+    })
+  };
+}
